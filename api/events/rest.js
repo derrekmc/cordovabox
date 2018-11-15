0e9b9946317baf139fbfa3b4e5b97353e1f78d69
Derrek <derrek_cordova@partech.com>
2018-11-14 20:21:19 -0800

    Removed events from hooks for now

--- api/events/rest.js
+++ api/events/rest.js
@@ -1,40 +1,40 @@
-class Rest {
-
-    constructor(){
-
-    }
-
-    get (route) {
-        //todo delete
-        log.debug(route, data);
-    }
-
-    put (route, data) {
-        //todo delete
-        log.debug(route, data);
-    }
-
-    post (route, data) {
-        //todo delete
-        log.debug(route, data);
-    }
-
-    delete (route, data) {
-        //todo delete
-        log.debug(route, data);
-    }
-};
-
-// module.exports = {
-//     HEARTBEAT: "heartbeat",
-//     MESSAGE: "msg",
-//     REQUEST: "request",
-//     WELCOME: "welcome",
-//     TIME: "time",
-//     COUNT: "count",
-//     REPLY: "reply",
-//     DATE: "date",
-//     ERROR: "error"
+// class Rest {
+//
+//     constructor(){
+//
+//     }
+//
+//     get (route) {
+//         //todo delete
+//         log.debug(route, data);
+//     }
+//
+//     put (route, data) {
+//         //todo delete
+//         log.debug(route, data);
+//     }
+//
+//     post (route, data) {
+//         //todo delete
+//         log.debug(route, data);
+//     }
+//
+//     delete (route, data) {
+//         //todo delete
+//         log.debug(route, data);
+//     }
 // };
-
-module.exports = new Rest();
+//
+// // module.exports = {
+// //     HEARTBEAT: "heartbeat",
+// //     MESSAGE: "msg",
+// //     REQUEST: "request",
+// //     WELCOME: "welcome",
+// //     TIME: "time",
+// //     COUNT: "count",
+// //     REPLY: "reply",
+// //     DATE: "date",
+// //     ERROR: "error"
+// // };
+//
+// module.exports = new Rest();
