4525bb3459c61bfc2a9f0c41228407e279dcac15
derrekmc <derrekmc@gmail.com>
2021-12-09 21:07:36 -0800

    Added open browser ability in dev

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const path = require("path");
 const server = require('./lib/app');
 const hooks = require('./lib/hooks/index');
+const open = require('open');
 async function start () {
     const port = process.env.PORT || 3000;
     try{
         await server.listen(port);
-        const open = require('open');
-        if(!process.env.NODE_ENV.includes('prod')) await open(`http://localhost:${port}`);
+        if(!String(process.env.NODE_ENV).includes('prod')) await open(`http://localhost:${port}`);
     }catch (err){
         console.error(err);
     }
