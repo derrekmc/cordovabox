2e65e6dfea84b8f00405a3c9d431e109dced64b4
derrekmc <derrekmc@gmail.com>
2025-10-17 15:19:17 -0700

    Added crud code as an example that can be overridden easily

--- bin/build.js
+++ bin/build.js
@@ -31,9 +31,61 @@ module.exports = {
         try {
             await fse.outputFile(controllerDirectory, `
 module.exports = {
+
     example: function(req, res){
         res.ok();
+    },
+    
+    /** These crud features are enabled by default but you can override them **/
+    /** To override them use the below to disable them do not just delete them because they will still work. You must keep the function but make it blank **/
+    
+    /*
+    get: async function (req, res){
+        let Model = global[req.modelName];
+        const query = {id: req.params['id']};
+        try{
+            const response = await Model.find(query)
+            res.status(200).send(response);
+        }catch(err){
+            res.status(403).send(err);
+        }
+    },
+
+    post: async function (req, res){
+        let Model = global[req.modelName];
+        const body = req.body;
+        const query = {id: req.params['id']};
+        try{
+            const response = await Model.create(body)
+            res.status(201).send(response);
+        }catch(err){
+            res.status(403).send(err);
+        }
+    },
+
+    put: async function (req, res){
+        let Model = global[req.modelName];
+        const body = req.body;
+        const query = {id: req.params['id']};
+        try{
+            const response = await Model.update(query, body)
+            res.status(203).send(response);
+        }catch(err){
+            res.status(403).send(err);
+        }
+    },
+
+    delete: async function (req, res){
+        let Model = global[req.modelName];
+        const query = {id: req.params['id']};
+        try{
+            const response = await Model.delete(query)
+            res.status(203).send(response);
+        }catch(err){
+            res.status(403).send(err);
+        }
     }
+    */
 };`)
 
         } catch (err) {
